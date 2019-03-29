216f9bf1efcbe378d2423bdb96cc9d462c70d179
Szymon Mentel <mentel.szymon@gmail.com>
2019-03-29 15:24:57 +0100

    Add consumer implementation

--- lib/rabbit_hole/consumer.ex
+++ lib/rabbit_hole/consumer.ex
@@ -3,20 +3,61 @@ defmodule RabbitHole.Consumer do
   Consumer API
   """
 
+  use GenServer
+  require Logger
+  alias RabbitHole.Protocol.{Connection, Channel, Queue, Basic}
+
+  defstruct conn: nil, chan: nil, queue: nil, consumer_tag: nil, processor: nil
+  alias __MODULE__, as: State
+
   @type consumer_ref :: any()
   @type processor :: (any() -> :ok | :error)
   @type opts() :: [processor: processor()]
 
+  # API
+
   @spec start(any(), opts()) :: {:ok, consumer_ref()}
-  def start(queue, opts \\ [])  do
-    # start the consumer as a separate process and return its pid as reference
-    # declare (assert) the queue
-    # consume messages from the queue and pass on to the processor fun
+  def start(queue, opts \\ []) do
+    GenServer.start(__MODULE__, [{:queue, queue} | opts])
   end
 
   @spec stop(consumer_ref()) :: :ok
   def stop(ref) do
-  # stop the consumer given its reference, usually a pid
+    GenServer.stop(ref)
   end
+
+  # CALLBACKS
+
+  def init(opts) do
+    {:ok, conn} = Connection.open()
+    {:ok, chan} = Channel.open(conn)
+    {:ok, _} = Queue.declare(chan, opts[:queue])
+    {:ok, tag} = Basic.consume(chan, opts[:queue])
+
+    {:ok,
+     %State{
+       conn: conn,
+       chan: chan,
+       queue: opts[:queue],
+       consumer_tag: tag,
+       processor: opts[:processor] || &default_processor/1
+     }}
+  end
+
+  def handle_info({:basic_deliver, message, _meta}, state) do
+    state.processor.(message)
+    Logger.info("Processed message: #{inspect message}")
+    {:noreply, state}
+  end
+
+  def terminate(_reason, state) do
+    {:ok, _} = Basic.cancel(state.chan, state.consumer_tag)
+    :ok = Channel.close(state.chan)
+    :ok = Connection.close(state.conn)
+  end
+
+  # HELPERS
+
+  defp default_processor(msg), do: IO.puts("Got message: #{inspect msg}")
 
 end
