7bbe963c9b5d2785b9c3455b39644a14e1e82393
Szymon Mentel <mentel.szymon@gmail.com>
2019-04-01 07:12:20 +0200

    Extend connection tests
    Add cases for multiple channels and close a connection with an open channel.

--- test/rabbit_hole/protocol/connection_test.exs
+++ test/rabbit_hole/protocol/connection_test.exs
@@ -1,6 +1,7 @@
 defmodule RabbitHole.Protocol.ConnectionTest do
   use ExUnit.Case
 
+  require Logger
   alias RabbitHole.Protocol.{Connection, Channel}
 
   test "connects and creates a channel" do
@@ -8,10 +9,45 @@ defmodule RabbitHole.Protocol.ConnectionTest do
     {:ok, _chan} = Channel.open(conn)
   end
 
-  test "connects, creates a channel and disconnects" do
-    {:ok, conn} = Connection.open()
-    {:ok, chann} = Channel.open(conn)
-    :ok = Channel.close(chann)
-    :ok = Connection.close(conn)
+  describe "with connection" do
+    setup do
+      {:ok, conn} = Connection.open()
+
+      on_exit(fn ->
+        try do
+          :ok = Connection.close(conn)
+        catch
+          :exit, _ ->
+            :ok
+            Logger.warn("The connection has already been closed")
+        end
+      end)
+
+      %{conn: conn}
+    end
+
+    test "creates a channel", params do
+      {:ok, chann} = Channel.open(params.conn)
+      :ok = Channel.close(chann)
+    end
+
+    test "connects, creates multiple channels and disconnects", params do
+      {:ok, chan1} = Channel.open(params.conn)
+      {:ok, chan2} = Channel.open(params.conn)
+      :ok = Channel.close(chan1)
+      :ok = Channel.close(chan2)
+    end
+
+    test "a channel is gone along with the connection", params do
+      {:ok, chann} = Channel.open(params.conn)
+      :ok = Connection.close(params.conn)
+
+      try do
+        :ok = Channel.close(chann)
+      catch
+        :exit, {:noproc, _} ->
+          :ok
+      end
+    end
   end
 end
